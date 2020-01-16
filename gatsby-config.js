faef0011e7368edf0f72fda6b82c1fea4f8c7ec1
北本涼生 <ryo_k12@kitamotoryousei-no-iMac.local>
2020-01-16 21:57:20 +0900

    add new file

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ const postCSSMixins = require('postcss-mixins')
 
 module.exports = {
   siteMetadata: {
-    title: `Hello Friend`,
+    title: `ちょこっと珈琲`,
     description: `A simple starter for Gatsby. That's it.`,
     copyrights: '',
     author: `@panr`,
@@ -15,7 +15,7 @@ module.exports = {
       src: '',
       alt: '',
     },
-    logoText: 'hello friend',
+    logoText: 'ちょこっと珈琲',
     defaultTheme: 'dark',
     postsPerPage: 5,
     showMenuItems: 2,
